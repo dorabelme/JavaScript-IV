8f25e2b9391179961cf98a669bac89ffcf393fe6
Dora Belme <contact.dorabelme@gmail.com>
2019-06-13 14:28:16 -0400

    added class Instructor and code from yesterday

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -10,13 +10,30 @@ const fred = new Instructor({
 });
 
 class Person {
-  constructor(attributes) {
-    this.name = attributes.name;
-    this.age = attributes.age;
-    this.location = attributes.location;
+  constructor(props) {
+    this.name = props.name;
+    this.age = props.age;
+    this.location = props.location;
   }
 
   speak() {
     console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+  }
+}
+
+class Instructor extends Person {
+  constructor(props) {
+    super(props);
+    this.specialty = props.specialty;
+    this.favLanguage = props.favLanguage;
+    this.catchPhrase = props.catchPhrase;
+  }
+
+  demo(subject) {
+    return `Today we are learning about ${this.subject}`;
+  }
+
+  grade(student) {
+    return `${this.student} receives a perfect score on ${this.subject}`;
   }
 }
