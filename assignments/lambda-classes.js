7e752a2b1b01c1490d3bba9e0562a80ee08224b5
Dora Belme <contact.dorabelme@gmail.com>
2019-06-13 15:06:26 -0400

    added tests for testing classes

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -7,7 +7,7 @@ class Person {
     this.location = props.location;
   }
   speak() {
-    console.log(`Hello my name is ${this.name}, I am from ${this.location}`);
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
   }
 }
 
@@ -19,10 +19,10 @@ class Instructor extends Person {
     this.catchPhrase = props.catchPhrase;
   }
   demo(subject) {
-    console.log(`Today we are learning about ${this.subject}`);
+    return `Today we are learning about ${subject}`;
   }
-  grade(student) {
-    console.log(`${this.student} receives a perfect score on ${this.subject}`);
+  grade(student, subject) {
+    return `${student} receives a perfect score on ${subject}`;
   }
 }
 
@@ -34,12 +34,13 @@ class Student extends Person {
     this.favSubjects = props.favSubjects;
   }
   listsSubjects() {
-    this.favSubjects.map(subject => console.log(subject));
+    return this.favSubjects;
   }
   PRAssignment(subject) {
-    console.log(
-      `${this.student.name} has begun sprint challenge on ${this.subject}`
-    );
+    return `${this.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge(subject) {
+    return `${this.name} has begun sprint challenge on ${subject}`;
   }
 }
 
@@ -50,14 +51,10 @@ class ProjectManager extends Instructor {
     this.favInstructor = props.favInstructor;
   }
   standUp(channel) {
-    console.log(
-      `${this.name} announces to ${this.channel} @channel standby times!`
-    );
+    return `${this.name} announces to ${channel} @channel standby times!`;
   }
   debugsCode(student, subject) {
-    console.log(
-      `${this.name} debugs ${this.student.name}'s code on ${this.subject}`
-    );
+    return `${this.name} debugs ${student}'s code on ${subject}`;
   }
 }
 
@@ -69,3 +66,29 @@ const fred = new Instructor({
   specialty: "Front-end",
   catchPhrase: `Don't forget the homies`
 });
+
+const dora = new Student({
+  name: "Dora",
+  location: "New York City",
+  previousBackground: "social sciences",
+  className: "Web21",
+  favSubjects: ["Data Sctructures", "Algorithms", "HTML and CSS"]
+});
+
+const henry = new ProjectManager({
+  name: "Henry",
+  location: "East Coast",
+  gradClassName: "Web?",
+  favInstructor: "Everyone"
+});
+
+console.log(fred.speak());
+console.log(fred.demo("JavaScript"));
+console.log(fred.grade("Dora", "HTML"));
+console.log(dora.listsSubjects());
+console.log(dora.PRAssignment("JavaScript"));
+console.log(dora.sprintChallenge("Intro to JavaScript"));
+
+console.log(henry.gradClassName);
+console.log(henry.standUp("web21_henry"));
+console.log(henry.debugsCode("Dakota", "JavaScript"));
