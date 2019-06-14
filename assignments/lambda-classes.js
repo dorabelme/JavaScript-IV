c053aaeb2b9081adfeddaef5bbfcacf7928cfbbd
Dora Belme <contact.dorabelme@gmail.com>
2019-06-13 20:20:45 -0400

    added better tests

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -72,6 +72,17 @@ class ProjectManager extends Instructor {
   }
 }
 
+// Instructors
+const dan = new Instructor({
+  name: "Dan",
+  age: "Infinity",
+  location: "Denver",
+  specialty: "Relentless Debugger",
+  favLanguage:
+    "JavaScript, Python, Elm, ReasonML, SmallTalk, Haskell, C#, Java, Rust, Go, Ruby, Crystal, Elixir, Lua, and Julia",
+  catchPhrase: "If you can do the thing, you can get paid to do the thing!"
+});
+
 const fred = new Instructor({
   name: "Fred",
   location: "Bedrock",
@@ -81,6 +92,7 @@ const fred = new Instructor({
   catchPhrase: `Don't forget the homies`
 });
 
+// Students
 const dora = new Student({
   name: "Dora",
   location: "New York City",
@@ -99,6 +111,41 @@ const strugglingstudent = new Student({
   grade: 60
 });
 
+const isaiah = new Student({
+  name: "Isaiah",
+  age: 18,
+  location: "Florida",
+  previousBackground: "High School last month",
+  className: "Web21",
+  favSubjects: ["Html", "CSS", "JavaScript"]
+});
+const kevin = new Student({
+  name: "Kevin",
+  age: 28,
+  location: "California",
+  previousBackground: "Table Games Dealer",
+  className: "WEB21",
+  favSubjects: ["Html", "CSS", "JavaScript"]
+});
+const nisa = new Student({
+  name: "Nisa",
+  age: 25,
+  location: "Ohio",
+  previousBackground: "Debt Collector",
+  className: "Web21",
+  favSubjects: ["Html", "CSS", "JavaScript"]
+});
+
+const joscelyn = new Student({
+  name: "Joscelyn",
+  age: 29,
+  location: "California",
+  previousBackground: "English teacher",
+  className: "Web21",
+  favSubjects: ["Computer Science", "Philosophy", "English"]
+});
+
+// PMs
 const henry = new ProjectManager({
   name: "Henry",
   location: "East Coast",
@@ -116,19 +163,92 @@ const austin = new ProjectManager({
   favLanguage: "Java",
   catchPhrase: ":eggplant:"
 });
+const marguel = new ProjectManager({
+  name: "Marguel",
+  age: "Maybe 26",
+  gradClassName: "WEBPT2",
+  favInstructor: "Me?",
+  location: "California",
+  specialty: "React",
+  favLanguage: "JavaScript, Python, Elm etc.",
+  catchPhrase: "Practice Flex Zombies !!!"
+});
 
-fred.speak();
-fred.demo("JavaScript");
-fred.grade(dora, "HTML");
-dora.listsSubjects();
-dora.PRAssignment("JavaScript");
-dora.sprintChallenge("Intro to JavaScript");
+const brandon = new ProjectManager({
+  name: "Brandon",
+  age: "34",
+  gradClassName: "WEB18",
+  favInstructor: "Professor Lambda",
+  location: "Maine",
+  specialty: "Redux",
+  favLanguage: "JavaScript, C++, Python.",
+  catchPhrase: "You shall not pass!"
+});
 
-console.log(henry.gradClassName);
-henry.standUp("web21_henry");
-henry.debugsCode(dora, "JavaScript");
-console.log(austin.favInstructor);
-console.log(austin.specialty);
+const mary = new ProjectManager({
+  name: "Mary",
+  age: "24",
+  gradClassName: "WEB18",
+  favInstructor: "Josh Knell",
+  location: "New York",
+  specialty: "Express and Node.js",
+  favLanguage: "Javascript",
+  catchPhrase: "That looks AWESOME"
+});
+
+const christian = new ProjectManager({
+  name: "Christian",
+  age: "32",
+  gradClassName: "WEB18",
+  favInstructor: "Every Instructor in Lambda",
+  location: "Seattle, WA",
+  specialty: "Data Structures & Algorithms",
+  favLanguage: "JavaScript",
+  catchPhrase: "Dont forget your daily commit."
+});
+
+const pat = new ProjectManager({
+  name: "Pat",
+  age: "38",
+  gradClassName: "WEB18",
+  favInstructor: "Brett Madrid",
+  location: "Petaluma, Ca",
+  specialty: "Empathetic to the struggle of Redux",
+  favLanguage: "JavaScript",
+  catchPhrase: "Lets google that together."
+});
+
+const darren = new ProjectManager({
+  name: "Darren",
+  age: "25",
+  gradClassName: "WEB18",
+  favInstructor: "Josh Knell",
+  location: "North Carolina",
+  specialty: "React",
+  favLanguage: "Javascript",
+  catchPhrase: "Gang. Gang."
+});
+
+const instructors = [dan, fred];
+const students = [dora, strugglingstudent, isaiah, kevin, nisa, joscelyn];
+const pms = [henry, austin, marguel, brandon, mary, christian, pat, darren];
+
+// console.log(instructors);
+// console.log(students);
+// console.log(pms);
+
+const everyone = instructors.concat(students).concat(pms);
+everyone.forEach(person => person.speak());
+instructors.forEach(instructor => instructor.demo("JavaScript"));
+instructors.forEach(instructor => instructor.grade(kevin, "JavaScript"));
+
+students.forEach(student => student.listsSubjects());
+students.forEach(student => student.PRAssignment("Classes"));
+students.forEach(student => student.sprintChallenge("Intro to JavaScript"));
+
+pms.forEach(pm => pm.standUp("web21_ah"));
+pms.forEach(pm => pm.debugsCode(nisa, "JavaScript"));
+
 console.log(fred.gradecreator(dora, 6));
 console.log(dora.graduate());
 console.log(strugglingstudent.grade);
